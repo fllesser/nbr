c6edbfb454476000f7e50b8356a5d6dd43ee454e
fllesser <fllessive@gmail.com>
2025-12-21 22:30:40 +0800

    tweak

--- tests/test_uv.rs
+++ tests/test_uv.rs
@@ -5,7 +5,6 @@ use nbr::uv;
 async fn test_uv_operations_snapshot() {
     let (_dir, project_path) = common::create_temp_project(true).await;
 
-    // Test show_package_info
     let package = uv::show_package_info("nonebot2", Some(&project_path))
         .await
         .unwrap();
@@ -13,16 +12,5 @@ async fn test_uv_operations_snapshot() {
     insta::assert_yaml_snapshot!("uv_show_package_info", package, {
         ".location" => "[LOCATION]",
         ".version" => "[VERSION]",
-    });
-
-    // Test list
-    let mut packages = uv::list(false).await.unwrap();
-    // Filter or truncate to make it more stable
-    packages.retain(|p| p.name == "nonebot2" || p.name.contains("adapter"));
-    packages.sort_by(|a, b| a.name.cmp(&b.name));
-
-    insta::assert_yaml_snapshot!("uv_list", packages, {
-        "[].location" => "[LOCATION]",
-        "[].version" => "[VERSION]",
     });
 }
