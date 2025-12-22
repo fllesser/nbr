585702167fbed61b33c3c26dde8e44d5cb619fad
fllesser <fllessive@gmail.com>
2025-12-22 15:25:55 +0800

    tweak

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use clap::Parser;
 use nbr::{cli::Cli, log, uv};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     let cli = Cli::parse();
     log::init_logging(cli.verbose);
+
+    if let Err(err) = run(cli).await {
+        tracing::error!("{err}");
+        std::process::exit(1);
+    }
+}
+
+async fn run(cli: Cli) -> anyhow::Result<()> {
     uv::self_version().await?;
     cli.run().await?;
     Ok(())
